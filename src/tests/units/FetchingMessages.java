052f326e41667042d341b9efccf270aa90353e4f
davidkron <david_jensen3@hotmail.com>
2015-02-04 21:33:08 +0100

    More tc

--- src/tests/units/FetchingMessages.java
+++ src/tests/units/FetchingMessages.java
@@ -1,27 +1,32 @@
 import junit.framework.TestCase;
 import org.junit.Test;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
+import java.io.IOException;
 import java.io.StringReader;
 
 public class FetchingMessages extends TestCase {
+
+    NodeList parseXMLString(String string) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        InputSource is = new InputSource();
+        is.setCharacterStream(new StringReader(string));
+        return db.parse(is).getDocumentElement().getChildNodes();
+    }
 
     //The xml string does not contain any message nodes when no message with the specified receipent ID exist.
     @Test
     public void testFetchMessageNotExists() throws Exception {
         String xmlreturn = Messages.fetch("0767731855");
-
-        DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        InputSource is = new InputSource();
-        is.setCharacterStream(new StringReader(xmlreturn));
-        Document doc = db.parse(is);// THE XML STRING IS VALID
-
-        NodeList nodeList = doc.getDocumentElement().getChildNodes();
+        NodeList nodeList = parseXMLString(xmlreturn);;
         assertTrue(nodeList.getLength() == 0);// THE XML STRING DOES NOT CONTAIN ANY MESSAGES
     }
 
@@ -42,12 +47,7 @@ public class FetchingMessages extends TestCase {
         Messages.fetch("0767731855");
         String xmlreturn = Messages.fetch("0767731855");
 
-        DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        InputSource is = new InputSource();
-        is.setCharacterStream(new StringReader(xmlreturn));
-        Document doc = db.parse(is);// THE XML STRING IS VALID
-
-        NodeList nodeList = doc.getDocumentElement().getChildNodes();
+        NodeList nodeList = parseXMLString(xmlreturn);// THE XML STRING IS VALID
         assertTrue(nodeList.getLength() == 0);// THE XML STRING DOES NOT CONTAIN ANY MESSAGES
     }
 
@@ -70,4 +70,32 @@ public class FetchingMessages extends TestCase {
             assertTrue(Messages.get(messageIds[i]).isfetching);
         }
     }
+
+    //The xml string contains message nodes when at least one unfetched message with the specified ID exists.
+    @Test
+    public void testContainsMessageOnSuccess() throws Exception {
+        String senderId = "0767731855";
+        String message = "Hello";
+
+        int messageIds[] = new int[10];
+
+        for(int i = 0; i <10; i++){
+            int id = Messages.add(message, senderId, "0767731855");
+            assertFalse(Messages.get(id).isfetching);
+            messageIds[i] = id;
+        }
+
+        String xmlreturn = Messages.fetch("0767731855");
+        NodeList nodeList = parseXMLString(xmlreturn);// THE XML STRING IS VALID
+        assertTrue(nodeList.getLength() == 0);// THE XML STRING DOES NOT CONTAIN ANY MESSAGES
+        for(int i = 0; i < nodeList.getLength(); i++){
+            assertTrue(nodeList.item(i).getNodeType() == Node.ELEMENT_NODE);
+            if(nodeList.item(i).getNodeName().equals("Message")){
+                assertEquals(nodeList.item(i).getFirstChild().getNodeValue(),message);
+            }
+        }
+    }
+
+
+
 }
