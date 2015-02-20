b82668c2fc60368905739e34ac04c1261146ff9f
davidkron <david_jensen3@hotmail.com>
2015-02-20 15:53:05 +0100

    Failing tc, yey!

--- src/tests/units/server/XMLEncoderTest.java
+++ src/tests/units/server/XMLEncoderTest.java
@@ -18,7 +18,7 @@ public class XMLEncoderTest {
         String ID = "0767731855";
         String xml = XMLEncoder.AcceptedConnection(Response.Connected(ID));
         String acceptString = "<connection><accepted>" + ID + "</accepted></connection>";
-        assertEquals(xml, acceptString);
+        assertEquals(acceptString,xml);
     }
 
     @Test
@@ -26,11 +26,17 @@ public class XMLEncoderTest {
         Integer ID = 5;
         String xml = XMLEncoder.Added(Response.Added(ID));
         String acceptString = "<messageActionResponse><added>" + ID + "</added></messageActionResponse>";
-        assertEquals(xml, acceptString);
+        assertEquals(acceptString,xml);
     }
 
 
-
+    @Test
+    public void testDeleteMessage() throws Exception {
+        Integer ID = 5;
+        String xml = XMLEncoder.Added(Response.Deleted(ID));
+        String acceptString = "<deleted>" + ID+ "</deleted>";
+        assertEquals(acceptString,xml);
+    }
         /*
 
         DELETE:
